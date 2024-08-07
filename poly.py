b778f6aa72a779fce5c1aaf9a8bfac581feaed2b
PurpleHyacinth120 <141423668+PurpleHyacinth120@users.noreply.github.com>
2024-08-07 20:46:47 +0530

    Add files via upload

---
+++ poly.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+im = cv2.imread("C:/Users/HP/Documents/.vscode/Edge AI/Annotation app/Images/aachen_000005_000019_leftImg8bit_foggy_beta_0.02_fake_0.png")
+
+plt.imshow(im)
+plt.show()
+# p = plt.ginput(2)
+
+# print(p)
